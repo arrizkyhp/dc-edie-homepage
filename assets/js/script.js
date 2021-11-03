5cf1e419096f95f83be9ab103fd76d9d6cf368d4
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2021-11-03 18:35:11 +0700

    done slicing and some js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,6 @@ const navbar = document.getElementById("navbar")
 
 const sticky = navbar.offsetTop;
 
-console.log(sticky)
-
 function scrollNav() {
     if (window.scrollY >= sticky + 100) {
       navbar.classList.add("sticky");
@@ -13,6 +11,35 @@ function scrollNav() {
     }
 }
 console.log(window.scrollY);
-window.onscroll = function () {
-  scrollNav();
-};
+
+
+// scroll active
+
+const sections = document.querySelectorAll("section");
+const navLink = document.querySelectorAll("nav .nav-menu li a");
+
+window.onscroll = () => {
+  let current = "";
+    scrollNav();
+
+    // console.log(document.body.offsetHeight);
+    // console.log(window.innerHeight + window.scrollY);
+    // console.log(document.body.offsetHeight);
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (scrollY >= sectionTop - 40) {
+      current = section.getAttribute("id");
+    }
+  })
+
+  navLink.forEach((anchor) => {
+    // console.log(current)
+    anchor.classList.remove("active");
+    if ((window.innerHeight + window.scrollY) === document.body.offsetHeight) {
+      navLink[3].classList.remove("active");
+      navLink[4].classList.add("active");
+    } if (anchor.classList.contains(current)) {
+      anchor.classList.add("active");
+    }
+  })
+}
